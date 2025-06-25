212dd9b56af29f9b2090f311e7c6af6c3a4377a6
Jacob Miller <58646480+jacobmiller9826@users.noreply.github.com>
2025-06-25 12:06:59 -0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const cacheName = "adwizard-v1";
+const assets = [
+  "/wizard.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
